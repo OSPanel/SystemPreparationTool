db9a3641ed469a8ec098802740360f64b164132a
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-10-03 09:43:26 +0300

    Update firewall.bat

--- resources/firewall.bat
+++ resources/firewall.bat
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee8a478f0db6eb83117fcd56f0a4feff33bbc634ee549316ab3a683b29e15a03
-size 749
+oid sha256:ce87cb5b770de6447b408296fe0a01f26e66f6e68fe86a5c61518746c45a3b67
+size 804
